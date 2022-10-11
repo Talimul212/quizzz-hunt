6f93db2d0d458d2c5cdbe56d7ca4500705ce4c83
Talimul212 <islamtalimul0@gmail.com>
2022-10-11 12:49:59 +0600

    options add and some css file

--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -1,11 +1,59 @@
-import React from 'react';
 
-const Question = ({ question }) => {
-    console.log(question);
+import Options from '../Options/Options';
+import './Question.css'
+import { EyeIcon } from '@heroicons/react/24/solid'
+const Question = ({ questions }) => {
+    const { question, correctAnswer, options } = questions;
+    const handlerRealAns = () => {
+        // console.log("hjfgjkfgj")
+        alert(`Answer: ${correctAnswer}`);
+    }
+    const handlerUserAns = () => {
+        console.log(options);
+        // const userAns0 = options[0].length;
+        // const userAns1 = options[1].length;
+        // const userAns2 = options[2].length;
+        // const userAns3 = options[3].length;
+        // const realAns = correctAnswer.length;
+        // if (userAns1 === realAns.length) {
+        //     alert('fhslkfgjlsd')
+        // }
+        // else {
+        //     alert('nnot correct')
+        // }
+        // console.log(userAns0, userAns1, userAns2, userAns3);
+        // if (userAns0 === realAns) {
+        //     alert("jfshkjlfgh")
+        //     console.log(userAns0, realAns);
+        // }
+        // else if (userAns1 !== realAns) {
+        //     alert("jfshkjlfgh")
+        //     console.log(userAns1, realAns);
+        // }
+        // else if (userAns2 !== realAns) {
+        //     alert("jfshkjlfgh")
+        //     console.log(userAns2, realAns);
+        // }
+        // else if (userAns3 !== realAns) {
+        //     alert("jfshkjlfgh")
+        //     console.log(userAns3, realAns);
+        // }
+        // else {
+        //     alert(" Correct Ans ")
+        // }
+
+    }
     return (
-        <div>
-            <h2>Length:{question.length}</h2>
-        </div>
+        <div className='question-container'>
+            <div className='quesxtion-title'>
+                <p></p>
+                <h3>Quiz:{question}</h3>
+                <EyeIcon onClick={() => handlerRealAns()} className='icon' />
+            </div>
+            <section>
+                <Options handlerUserAns={handlerUserAns} options={options}></Options>
+            </section>
+        </div >
     );
 };
 
--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom'
+import Question from '../Question/Question';
 const Quiz = () => {
     const quiz = useLoaderData();
     const questionList = quiz.data.questions;
-    console.log(questionList);
     return (
         <div>
-            {/* {
-                quiz.data.questions.map(question => <Quiz question={question}></Quiz>)
-            } */}
+            {
+                questionList.map(questions => <Question key={questions.id} questions={questions}></Question>)
+            }
         </div>
     );
 };
